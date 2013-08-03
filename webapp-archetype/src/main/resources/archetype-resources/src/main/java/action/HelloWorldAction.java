6e7ded6e7955c7fe7263a4492ca99ab0366eea9c
wojniemiec <woj.niemiec@gmail.com>
2013-08-03 12:30:07 +0200

    webapp archetype uses now Bristleback 0.3.5

--- webapp-archetype/src/main/resources/archetype-resources/src/main/java/action/HelloWorldAction.java
+++ webapp-archetype/src/main/resources/archetype-resources/src/main/java/action/HelloWorldAction.java
@@ -9,7 +9,6 @@ import pl.bristleback.server.bristle.api.action.DefaultAction;
 import pl.bristleback.server.bristle.api.annotations.Action;
 import pl.bristleback.server.bristle.api.annotations.ActionClass;
 import pl.bristleback.server.bristle.engine.user.BaseUserContext;
-import pl.bristleback.server.bristle.serialization.system.annotation.Bind;
 
 @ActionClass(name = "HelloWorld")
 @Controller
@@ -19,12 +18,12 @@ public class HelloWorldAction implements DefaultAction<BaseUserContext, String> 
   private SampleClientAction sampleClientAction;
 
   @Action
-  public String executeDefault(BaseUserContext defaultUser, @Bind(required = true) String text) {
+  public String executeDefault(BaseUserContext defaultUser, String text) {
     return new StringBuilder(text).reverse().toString();
   }
 
   @Action
-  public String duplicateText(@Bind(required = true) String text) {
+  public String duplicateText(String text) {
     return text + text;
   }
 
